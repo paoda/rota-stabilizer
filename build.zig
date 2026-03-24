41a64750c7524597fefbdde9c54ba20a55f88c96
paoda <rekai@musuka.dev>
2026-03-24 15:18:45 -0500

    chore: update binary name

--- build.zig
+++ build.zig
@@ -76,7 +76,7 @@ pub fn build(b: *std.Build) !void {
     // This creates another `std.Build.Step.Compile`, but this one builds an executable
     // rather than a static library.
     const exe = b.addExecutable(.{
-        .name = "rota_stabilizer",
+        .name = "rota-stabilizer",
         .root_module = exe_mod,
         .use_llvm = true,
     });
